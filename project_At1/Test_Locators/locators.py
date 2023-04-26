6ef687c50f875866b216c19639ed25eb5e0de16e
Shreegithubid <125814044+Shreegithubid@users.noreply.github.com>
2023-04-26 11:35:42 +0530

    Update locators.py

--- project_At1/Test_Locators/locators.py
+++ project_At1/Test_Locators/locators.py
@@ -1,6 +1,7 @@
 # locators.py - File where all The HTML Locators are Kept   
 class Hrm_Locators:    
-#Locators for Test case ID: Tc_LOGIN_01   
+#Locators for Test case ID: Tc_LOGIN_01 
+#Locators for Test case ID: Tc_LOGIN_02   
    
     username_inputBox = 'username'
 
@@ -8,21 +9,8 @@ class Hrm_Locators:
     
     LoginButton ='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
     
-#Locators for Test case ID: Tc_LOGIN_02   
-class Hrm1_Locators:  
-    username_inputBox = 'username'
-
-    password_inputBox = 'password'
-    
-    LoginButton ='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
 
 #Locators for Test case ID: Tc_PIM_01 
-class Hrm2_Locators:
-    username_inputBox = 'username'
-
-    password_inputBox = 'password'
-    
-    LoginButton ='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
         
     pimbutton    ='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'
        
@@ -35,14 +23,6 @@ class Hrm2_Locators:
     save_button1 = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/form/div[2]/button[2]'
     
 #Locators for Test case ID: Tc_PIM_02
-class Hrm3_Locators:
-    username_inputBox = 'username'
-
-    password_inputBox = 'password'
-    
-    LoginButton ='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
-        
-    pimbutton    ='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'
            
     employee_name = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input'
     
@@ -55,18 +35,6 @@ class Hrm3_Locators:
     saveicon = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/form/div[5]/button'
 
 #Locators for Test case ID: Tc_PIM_03
-class Hrm4_Locators:
-    username_inputBox = 'username'
-
-    password_inputBox = 'password'
-    
-    LoginButton ='//*[@id="app"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button'
-        
-    pimbutton    ='//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a'
-    
-    employee_name = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[1]/div/div[1]/div/div[2]/div/div/input'
- 
-    search_button = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[1]/div[2]/form/div[2]/button[2]'
     
     delete_button = '//*[@id="app"]/div[1]/div[2]/div[2]/div/div[2]/div[3]/div/div[2]/div[1]/div/div[9]/div/button[1]/i'
     
