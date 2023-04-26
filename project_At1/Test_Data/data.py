d7482be0ff267421fd27da4be66b9db5c7d098d5
Shreegithubid <125814044+Shreegithubid@users.noreply.github.com>
2023-04-26 11:29:20 +0530

    Update data.py

--- project_At1/Test_Data/data.py
+++ project_At1/Test_Data/data.py
@@ -10,46 +10,22 @@ class Hrm_Data:
     password = 'admin123'  
 
 # Data for Test case ID: Tc_LOGIN_02
-class Hrm1_Data:    
-    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    
-    user_name = 'Admin'
 
     pass_word = 'admin124'  
   
 # Data for Test case ID: Tc_PIM_01  
-class Hrm2_Data:
-    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    
-    username = 'Admin'
 
-    password = 'admin123' 
-  
     name = 'shreenath'
     
     lastname = 'R'
     
 # Data for Test case ID: Tc_PIM_02            
-class Hrm3_Data:
-    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    
-    username = 'Admin'
-
-    password = 'admin123' 
     
     employee_name = 'shreenath'
     
     midname = 'shree' 
     
 
-class Hrm4_Data:
-    url = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
-    
-    username = 'Admin'
-
-    password = 'admin123'  
-    
-    employee_name = 'shreenath'   
     
     
     
